d75caf6d4c3a1c8dbe636e80c6b895d362530295
Deeptimayee ojha <ojhadeeptymayee@gmail.com>
2026-01-15 08:32:28 +0530

    ImageCompressor

---
+++ ImageCompressor/image.py
@@ -0,0 +1,13 @@
+import PIL
+from PIL import Image
+from tkinter.filedialog import *
+
+file_path = askopenfilename()
+img = PIL.Image.open(file_path)
+myHeight, myWidth = img.size
+
+img = img.resize((myWidth //2, myHeight//2), PIL.Image.Resampling.LANCZOS)
+save_path = asksaveasfilename()
+
+img.save(save_path+"_compressed.jpg")
+
